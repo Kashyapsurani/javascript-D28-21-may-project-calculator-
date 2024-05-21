2498ead4fee32c278fb154a0b946cff45e3c3733
Kashyap surani <152683782+Kashyapsurani@users.noreply.github.com>
2024-05-21 18:18:07 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+var button = document.querySelectorAll("button")
+var h1 = document.querySelector(".h1")
+
+var str = "";
+
+button.forEach((value) => {
+    value.addEventListener("click", () => {
+
+        if (value.innerText == "Clar") {
+            str = ""
+            h1.innerHTML = 0
+        }
+        else if (value.innerText != "=") {
+            h1.innerText = str += value.innerText
+        } else if (value.innerText == "=") {
+            h1.innerText = eval(str);
+        }
+
+    })
+
+})
