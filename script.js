29445b78ab4038fb2360289d4efbe6c5d1de6154
Kashyap surani <152683782+Kashyapsurani@users.noreply.github.com>
2024-06-10 08:16:39 +0530

    Calculetar

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-var button = document.querySelectorAll("button")
-var h1 = document.querySelector(".h1")
+const outputEl = document.querySelector(".text");
+const valEl = document.querySelectorAll(".val");
+const ClearEl = document.querySelector(".AC");
+const deleteEl = document.querySelector(".DEL");
+const equalEl = document.querySelector(".equal");
 
-var str = "";
-
-button.forEach((value) => {
-    value.addEventListener("click", () => {
-
-        if (value.innerText == "Clar") {
-            str = ""
-            h1.innerHTML = 0
-        }else if (value.innerText != "=") {
-            h1.innerText = str += value.innerText
-
-        }else if (value.innerText == "=") {
-            h1.innerText = eval(str);
-            str = ""
-        }
-
+// It adds every value and display at the output
+valEl.forEach((val) => {
+    val.addEventListener("click", () => {
+        outputEl.value += val.value;
     })
+})
+
+// It clears all the digits in the output
+ClearEl.addEventListener("click", () => {
+    outputEl.value = "";
+})
+
+// It deletes the last digit in the output
+deleteEl.addEventListener("click", () => {
+    outputEl.value = outputEl.value.slice(0, -1);
+})
+
+// If there is no error, it evaluates the output value or else it shows syntax error as a alert message.
+equalEl.addEventListener("click", () => {
+    try {
+        outputEl.value = eval(outputEl.value);
+    } catch (e) {
+        alert(e);
+    }
 })
